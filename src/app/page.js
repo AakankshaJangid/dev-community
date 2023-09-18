3e5c2aef104722bb9f6658e027941b1efa5abab1
Aakanksha Jangid <aakanksha@incentiv.finance>
2023-09-18 17:47:17 +0530

    FQAS and Video tutorials created.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import LandingPage from '../../pages/LandingPage'
+'use client';
+import FQASection from './FQASection/page'
+import VideoTutorials from './video-tutorials/page';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
-      <LandingPage />
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-slate-800">
+      {/* <FQASection /> */}
+      <VideoTutorials />
     </main>
   )
 }
