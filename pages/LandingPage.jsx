cd41a03280be75ee4fcdfb5b1fcddbb7a3c2f683
Anshika <ananyapandey140101@gmail.com>
2023-09-20 17:50:05 +0530

    msg

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import TaskTracker from './TaskTracker'
+import Roadmap from './Roadmap'
 
 const Home = () => {
   return (
     <div>
       Hello
       Everyone
+      <Roadmap />
     </div>
   )
 }
 
 export default Home
+
